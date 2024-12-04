2f87e615e80fa2c79db2b382c4661fd8b5c4b243
Muhammad Awaludin Jamil <156278167+awaludinmil@users.noreply.github.com>
2024-12-04 14:52:22 +0700

    SCRUM-4 Update profile.dart

--- lib/views/profile.dart
+++ lib/views/profile.dart
@@ -1,6 +1,25 @@
-import 'package:flutter/material.dart';
+// lib/views/user.dart
 
-class ProfilePage extends StatelessWidget {
+import 'package:flutter/material.dart';
+import '../controllers/userController.dart';
+import '../models/userModel.dart';
+
+class UserPage extends StatefulWidget {
+  const UserPage({super.key});
+
+  @override
+  _UserPageState createState() => _UserPageState();
+}
+
+class _UserPageState extends State<UserPage> {
+  late Future<UserModel?> _userFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    _userFuture = UserController.getUser(context); // Pass context to UserController
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,12 +33,12 @@ class ProfilePage extends StatelessWidget {
           fit: BoxFit.contain,
         ),
         centerTitle: true,
-        actions: [
+        actions: const [
           SizedBox(width: 24),
         ],
       ),
       body: Container(
-        decoration: BoxDecoration(
+        decoration: const BoxDecoration(
           gradient: LinearGradient(
             begin: Alignment.topCenter,
             end: Alignment.bottomCenter,
@@ -31,148 +50,160 @@ class ProfilePage extends StatelessWidget {
           ),
         ),
         child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 20.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(height: 20),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Icon(Icons.delete_outline, color: Colors.grey),
-                  Text(
-                    'PROFILE',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
-                      letterSpacing: 1,
-                    ),
-                  ),
-                  SizedBox(width: 24),
-                ],
-              ),
-              SizedBox(height: 20),
-              Center(
-                child: Column(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          child: FutureBuilder<UserModel?>(
+            future: _userFuture,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(child: CircularProgressIndicator());
+              } else if (snapshot.hasError) {
+                return Center(child: Text('Error: ${snapshot.error}'));
+              } else if (!snapshot.hasData) {
+                return const Center(child: Text('Tidak ada data profil'));
+              } else {
+                UserModel? user = snapshot.data;
+                return ListView(
                   children: [
-                    Stack(
+                    const SizedBox(height: 20),
+                    const Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        Container(
-                          decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            border: Border.all(
-                              color: Color(0xFFFFB703),
-                              width: 2,
-                            ),
-                          ),
-                          child: CircleAvatar(
-                            radius: 60.0,
-                            backgroundImage:
-                                AssetImage('assets/profile_image.png'),
-                          ),
-                        ),
-                        Positioned(
-                          bottom: 0,
-                          right: 0,
-                          child: Container(
-                            padding: EdgeInsets.all(4),
-                            decoration: BoxDecoration(
-                              color: Color(0xFFFFB703),
-                              shape: BoxShape.circle,
-                            ),
-                            child: Icon(
-                              Icons.camera_alt,
-                              color: Colors.white,
-                              size: 20,
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                    SizedBox(height: 35),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
+                        Icon(Icons.delete_outline, color: Colors.grey),
                         Text(
-                          'Jhon Doe',
+                          'USER',
                           style: TextStyle(
                             color: Colors.white,
-                            fontSize: 24.0,
-                            fontWeight: FontWeight.w500,
-                            shadows: [
-                              Shadow(
-                                blurRadius: 10.0,
-                                color: Color(0xFFFFB703),
-                                offset: Offset(0, 0),
+                            fontSize: 20,
+                            letterSpacing: 1,
+                          ),
+                        ),
+                        SizedBox(width: 24),
+                      ],
+                    ),
+                    const SizedBox(height: 20),
+                    Center(
+                      child: Column(
+                        children: [
+                          Stack(
+                            children: [
+                              Container(
+                                decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  border: Border.all(
+                                    color: const Color(0xFFFFB703),
+                                    width: 2,
+                                  ),
+                                ),
+                                child: const CircleAvatar(
+                                  radius: 60.0,
+                                  backgroundImage: AssetImage('assets/user_image.png'),
+                                ),
+                              ),
+                              Positioned(
+                                bottom: 0,
+                                right: 0,
+                                child: Container(
+                                  padding: const EdgeInsets.all(4),
+                                  decoration: const BoxDecoration(
+                                    color: Color(0xFFFFB703),
+                                    shape: BoxShape.circle,
+                                  ),
+                                  child: const Icon(
+                                    Icons.camera_alt,
+                                    color: Colors.white,
+                                    size: 20,
+                                  ),
+                                ),
                               ),
                             ],
                           ),
-                        ),
-                        SizedBox(width: 8),
-                        Icon(
-                          Icons.edit,
+                          const SizedBox(height: 35),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                user!.name,
+                                style: const TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 24.0,
+                                  fontWeight: FontWeight.w500,
+                                  shadows: [
+                                    Shadow(
+                                      blurRadius: 10.0,
+                                      color: Color(0xFFFFB703),
+                                      offset: Offset(0, 0),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              const SizedBox(width: 8),
+                              Icon(
+                                Icons.edit,
+                                color: Colors.grey[600],
+                                size: 20,
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    Divider(color: Colors.grey[800]),
+                    ListTile(
+                      contentPadding: const EdgeInsets.symmetric(horizontal: 20.0),
+                      minLeadingWidth: 20,
+                      leading: Icon(Icons.phone, color: Colors.grey[600], size: 20),
+                      title: Text(
+                        user.phoneNumber,
+                        style: TextStyle(
                           color: Colors.grey[600],
-                          size: 20,
+                          fontSize: 20,
                         ),
-                      ],
+                      ),
+                    ),
+                    ListTile(
+                      contentPadding: const EdgeInsets.symmetric(horizontal: 20.0),
+                      minLeadingWidth: 20,
+                      leading: Icon(Icons.email, color: Colors.grey[600], size: 20),
+                      title: Text(
+                        user.email,
+                        style: TextStyle(
+                          color: Colors.grey[600],
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
+                    const Spacer(),
+                    Divider(color: Colors.grey[800]),
+                    Container(
+                      width: double.infinity,
+                      margin: const EdgeInsets.symmetric(vertical: 20),
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color.fromARGB(33, 255, 0, 0),
+                          padding: const EdgeInsets.symmetric(vertical: 15),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            side: BorderSide(color: Colors.red[900]!, width: 1),
+                          ),
+                          elevation: 0,
+                        ),
+                        onPressed: () {
+                          // Implementasi logout
+                        },
+                        child: const Text(
+                          'Log Out',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
                     ),
                   ],
-                ),
-              ),
-              SizedBox(height: 8),
-              Divider(color: Colors.grey[800]),
-              ListTile(
-                contentPadding: EdgeInsets.symmetric(horizontal: 20.0),
-                minLeadingWidth: 20,
-                leading: Icon(Icons.phone, color: Colors.grey[600], size: 20),
-                title: Text(
-                  '+62xxxxxxxxxx',
-                  style: TextStyle(
-                    color: Colors.grey[600],
-                    fontSize: 20,
-                  ),
-                ),
-              ),
-              ListTile(
-                contentPadding: EdgeInsets.symmetric(horizontal: 20.0),
-                minLeadingWidth: 20,
-                leading: Icon(Icons.email, color: Colors.grey[600], size: 20),
-                title: Text(
-                  'jhondoe@gmail.com',
-                  style: TextStyle(
-                    color: Colors.grey[600],
-                    fontSize: 20,
-                  ),
-                ),
-              ),
-              Spacer(),
-              Divider(color: Colors.grey[800]),
-              Container(
-                width: double.infinity,
-                margin: EdgeInsets.symmetric(vertical: 20),
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Color.fromARGB(33, 255, 0, 0),
-                    padding: EdgeInsets.symmetric(vertical: 15),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      side: BorderSide(color: Colors.red[900]!, width: 1),
-                    ),
-                    elevation: 0,
-                  ),
-                  onPressed: () {
-                    // Implementasi logout
-                  },
-                  child: Text(
-                    'Log Out',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 16,
-                    ),
-                  ),
-                ),
-              ),
-            ],
+                );
+              }
+            },
           ),
         ),
       ),
